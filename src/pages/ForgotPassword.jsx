f1f7e508d6093c95214306607cab7ac9f3f17fa4
alvi-sh <alvishstha97@gmail.com>
2025-06-12 15:59:56 +0545

    fixed an error for navigating

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -1,11 +1,11 @@
 import React, { useContext, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import DashboardForm from '../components/auth/DashboardForm';
+import ForgotPasswordForm from '../components/auth/ForgotPasswordForm';
 
 export default function ForgotPassword() {
     return (
         <div>
-            <DashboardForm />
+            <ForgotPasswordForm />
         </div>
     );
 }
